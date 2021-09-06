e77167631087a200961e61e8efbbc0fdd220c915
Kirsten Shyu <kirsten.shyu00@myhunter.cuny.edu>
2021-09-06 04:10:31 -0400

    update

--- smaller3.cpp
+++ smaller3.cpp
@@ -8,6 +8,7 @@ Assignment: Lab1B
 
 Write a program smaller3.cpp that asks the user to input three integer numbers,
  and prints out the smallest of the three.
+ 
 */
 #include <iostream>
 using namespace std;
